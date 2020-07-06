8debcaa998c9fd98bb2336396213b260d0365f09
Mauricio Ponce Oyarzún <mauricio.ponce.o@gmail.com>
2020-07-06 18:22:43 -0400

    [ADD] GridLayout

--- app/src/main/java/com/eme/android/java/views/MyCustomView.java
+++ app/src/main/java/com/eme/android/java/views/MyCustomView.java
@@ -11,6 +11,7 @@ import androidx.annotation.Nullable;
 
 import timber.log.Timber;
 
+
 @SuppressLint("AppCompatCustomView")
 public class MyCustomView extends TextView {
 
@@ -20,9 +21,28 @@ public class MyCustomView extends TextView {
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        Timber.d("onTouchEvent " + event.toString());
-        decorateText();
-        return super.onTouchEvent(event);
+        int action = event.getAction();
+
+        switch (action) {
+            case (MotionEvent.ACTION_DOWN):
+                Timber.d("Action was DOWN");
+                return true;
+            case (MotionEvent.ACTION_MOVE):
+                Timber.d("Action was MOVE");
+                return true;
+            case (MotionEvent.ACTION_UP):
+                Timber.d("Action was UP");
+                return true;
+            case (MotionEvent.ACTION_CANCEL):
+                Timber.d("Action was CANCEL");
+                return true;
+            case (MotionEvent.ACTION_OUTSIDE):
+                Timber.d("Movement occurred outside bounds " +
+                        "of current screen element");
+                return true;
+            default:
+                return super.onTouchEvent(event);
+        }
     }
 
     private void decorateText() {
