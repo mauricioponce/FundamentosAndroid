71fb338ae25ba7751a527b4022942f26761e660f
Mauricio Ponce Oyarzún <mauricio.ponce.o@gmail.com>
2020-07-09 08:56:38 -0400

    [CHG] MainActivity usando setContentView

--- app/src/main/java/com/eme/android/java/MainActivity.java
+++ app/src/main/java/com/eme/android/java/MainActivity.java
@@ -1,26 +1,20 @@
 package com.eme.android.java;
 
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.eme.android.java.databinding.ActivityMainBinding;
 
 import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
         initLog();
-
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
     }
 
     private void initLog() {
